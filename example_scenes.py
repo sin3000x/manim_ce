5db9df61253903f6f0dfe5dcb85115d683194516
Jeremy <sin3000x@gmail.com>
2025-01-11 13:44:03 +0800

    first commit: add example scenes

---
+++ example_scenes.py
@@ -0,0 +1,50 @@
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+        self.wait()
+
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(Transform(square, circle))  # transform the square into a circle
+        self.play(
+            square.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+
+
+class MarkupTest(Scene):
+    def construct(self):
+        text = MarkupText(
+            f'<span underline="double" underline_color="green">double green underline</span> in red text<span fgcolor="{YELLOW}"> except this</span>',
+            color=RED,
+            font_size=34
+        )
+        self.add(text)
+
+
+class CorrectLaTeXSubstringColoring(Scene):
+    def construct(self):
+        equation = MathTex(
+            r"e^x = x^0 + x^1 + \frac{1}{2} x^2 + \frac{1}{6} x^3 + \cdots + \frac{1}{n!} x^n + \cdots",
+            substrings_to_isolate="x"
+        )
+        equation.set_color_by_tex("x", YELLOW)
+        self.add(equation)
+
+
+class MathTeXDemo(Scene):
+    def construct(self):
+        rtarrow0 = MathTex(r"\xrightarrow{x^6y^8}", font_size=96)
+        rtarrow1 = Tex(r"$\xrightarrow{x^6y^8}$", font_size=96)
+
+        self.add(VGroup(rtarrow0, rtarrow1).arrange(DOWN))
